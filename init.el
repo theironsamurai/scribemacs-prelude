08946312b372139dda945486c9c5d265794eccd0
theironsamurai <shogunstrength@gmail.com>
2014-05-13 09:49:47 -0400

    forking another literate emacs conf

--- init.el
+++ init.el
@@ -1,2 +1,17 @@
-;;; init.el -- basic loading of the core Scribemacs.org file
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+
+(when (not (cl-remove-if-not 
+	    (lambda (p) (equal 'org (car p)))
+	    package-alist))
+  (message "No org-mode package found; installing now...")
+  (package-install 'org))
+
+(require 'org)
+(when (string-match "^[1234567]" (org-version))
+  (warn "Org-Mode is out of date. We expect org 8 or higher, but instead we have %s" (org-version)))
+
+(require 'pallet)
+
+(org-babel-load-file "~/.emacs.d/emacs.org")
 
